5cc057ad469fe6a373c51d7c56dd85f4157c015b
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-08-24 16:34:38 +0900

    first

---
+++ ppxx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ppxx
+//
+//  Created by 伊勢屋輝 on 2020/08/24.
+//  Copyright © 2020 伊勢屋輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
