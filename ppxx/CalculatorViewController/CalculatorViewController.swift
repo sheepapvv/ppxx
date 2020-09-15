8e319ff6cf9d05a7067abc8c02df1dd32595854e
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-09-15 16:08:37 +0900

    一応commit

---
+++ ppxx/CalculatorViewController/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorViewController.swift
+//  ppxx
+//
+//  Created by 伊勢屋輝 on 2020/09/15.
+//  Copyright © 2020 伊勢屋輝. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
