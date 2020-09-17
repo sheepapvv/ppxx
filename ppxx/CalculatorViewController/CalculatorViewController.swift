175a484a922ff890d9e48a312491ab81d43c3d39
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-09-17 20:28:52 +0900

    layout

--- ppxx/CalculatorViewController/CalculatorViewController.swift
+++ ppxx/CalculatorViewController/CalculatorViewController.swift
@@ -14,7 +14,17 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
+    
 
 
     /*
