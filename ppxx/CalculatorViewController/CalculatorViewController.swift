cb8f5b3cec85f8411d5c0df5654ffe160fee6e53
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-10-04 02:51:12 +0900

    部品名を変更

--- ppxx/CalculatorViewController/CalculatorViewController.swift
+++ ppxx/CalculatorViewController/CalculatorViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    @IBOutlet weak var button_0: UIButton!
-    @IBOutlet weak var button_1: UIButton!
-    @IBOutlet weak var button_2: UIButton!
-    @IBOutlet weak var button_3: UIButton!
-    @IBOutlet weak var button_4: UIButton!
-    @IBOutlet weak var button_5: UIButton!
-    @IBOutlet weak var button_6: UIButton!
-    @IBOutlet weak var button_7: UIButton!
-    @IBOutlet weak var button_8: UIButton!
-    @IBOutlet weak var button_9: UIButton!
+    @IBOutlet weak var periodButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
     
-    @IBOutlet weak var button_period: UIButton!
-    @IBOutlet weak var button_equal: UIButton!
-    @IBOutlet weak var button_plus: UIButton!
-    @IBOutlet weak var button_minus: UIButton!
-    @IBOutlet weak var button_multi: UIButton!
-    @IBOutlet weak var button_clear: UIButton!
-    @IBOutlet weak var button_negative: UIButton!
-    @IBOutlet weak var button_percent: UIButton!
-    @IBOutlet weak var button_divid: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multiButton: UIButton!
+    @IBOutlet weak var dividButton: UIButton!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var negativeButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
     
     
     @IBOutlet weak var answer_Label: UILabel!
@@ -40,108 +41,105 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        button_period.backgroundColor = UIColor.lightGray
-        button_period.setTitleColor(.black, for: .normal)
-        button_period.layer.borderColor = UIColor.black.cgColor
-        button_period.layer.borderWidth = 0.5
+        periodButton.backgroundColor = UIColor.lightGray
+        periodButton.setTitleColor(.black, for: .normal)
+        periodButton.layer.borderColor = UIColor.black.cgColor
+        periodButton.layer.borderWidth = 0.5
         
+        zeroButton.backgroundColor = UIColor.lightGray
+        zeroButton.setTitleColor(.black, for: .normal)
+        zeroButton.layer.borderColor = UIColor.black.cgColor
+        zeroButton.layer.borderWidth = 0.5
         
-        button_0.backgroundColor = UIColor.lightGray
-        button_0.setTitleColor(.black, for: .normal)
-        button_0.layer.borderColor = UIColor.black.cgColor
-        button_0.layer.borderWidth = 0.5
+        oneButton.backgroundColor = UIColor.lightGray
+        oneButton.setTitleColor(.black, for: .normal)
+        oneButton.layer.borderColor = UIColor.black.cgColor
+        oneButton.layer.borderWidth = 0.5
         
+        twoButton.backgroundColor = UIColor.lightGray
+        twoButton.setTitleColor(.black, for: .normal)
+        twoButton.layer.borderColor = UIColor.black.cgColor
+        twoButton.layer.borderWidth = 0.5
         
-        button_1.backgroundColor = UIColor.lightGray
-        button_1.setTitleColor(.black, for: .normal)
-        button_1.layer.borderColor = UIColor.black.cgColor
-        button_1.layer.borderWidth = 0.5
+        threeButton.backgroundColor = UIColor.lightGray
+        threeButton.setTitleColor(.black, for: .normal)
+        threeButton.layer.borderColor = UIColor.black.cgColor
+        threeButton.layer.borderWidth = 0.5
         
-        button_2.backgroundColor = UIColor.lightGray
-        button_2.setTitleColor(.black, for: .normal)
-        button_2.layer.borderColor = UIColor.black.cgColor
-        button_2.layer.borderWidth = 0.5
+        fourButton.backgroundColor = UIColor.lightGray
+        fourButton.setTitleColor(.black, for: .normal)
+        fourButton.layer.borderColor = UIColor.black.cgColor
+        fourButton.layer.borderWidth = 0.5
         
-        button_3.backgroundColor = UIColor.lightGray
-        button_3.setTitleColor(.black, for: .normal)
-        button_3.layer.borderColor = UIColor.black.cgColor
-        button_3.layer.borderWidth = 0.5
+        fiveButton.backgroundColor = UIColor.lightGray
+        fiveButton.setTitleColor(.black, for: .normal)
+        fiveButton.layer.borderColor = UIColor.black.cgColor
+        fiveButton.layer.borderWidth = 0.5
         
-        button_4.backgroundColor = UIColor.lightGray
-        button_4.setTitleColor(.black, for: .normal)
-        button_4.layer.borderColor = UIColor.black.cgColor
-        button_4.layer.borderWidth = 0.5
+        sixButton.backgroundColor = UIColor.lightGray
+        sixButton.setTitleColor(.black, for: .normal)
+        sixButton.layer.borderColor = UIColor.black.cgColor
+        sixButton.layer.borderWidth = 0.5
         
-        button_5.backgroundColor = UIColor.lightGray
-        button_5.setTitleColor(.black, for: .normal)
-        button_5.layer.borderColor = UIColor.black.cgColor
-        button_5.layer.borderWidth = 0.5
+        sevenButton.backgroundColor = UIColor.lightGray
+        sevenButton.setTitleColor(.black, for: .normal)
+        sevenButton.layer.borderColor = UIColor.black.cgColor
+        sevenButton.layer.borderWidth = 0.5
         
-        button_6.backgroundColor = UIColor.lightGray
-        button_6.setTitleColor(.black, for: .normal)
-        button_6.layer.borderColor = UIColor.black.cgColor
-        button_6.layer.borderWidth = 0.5
+        eightButton.backgroundColor = UIColor.lightGray
+        eightButton.setTitleColor(.black, for: .normal)
+        eightButton.layer.borderColor = UIColor.black.cgColor
+        eightButton.layer.borderWidth = 0.5
         
-        button_7.backgroundColor = UIColor.lightGray
-        button_7.setTitleColor(.black, for: .normal)
-        button_7.layer.borderColor = UIColor.black.cgColor
-        button_7.layer.borderWidth = 0.5
-        
-        button_8.backgroundColor = UIColor.lightGray
-        button_8.setTitleColor(.black, for: .normal)
-        button_8.layer.borderColor = UIColor.black.cgColor
-        button_8.layer.borderWidth = 0.5
-        
-        button_9.backgroundColor = UIColor.lightGray
-        button_9.setTitleColor(.black, for: .normal)
-        button_9.layer.borderColor = UIColor.black.cgColor
-        button_9.layer.borderWidth = 0.5
-        
-        
-        
-        button_clear.backgroundColor = UIColor.gray
-        button_clear.setTitleColor(.black, for: .normal)
-        button_clear.layer.borderColor = UIColor.black.cgColor
-        button_clear.layer.borderWidth = 0.5
-        
-        button_negative.backgroundColor = UIColor.gray
-        button_negative.setTitleColor(.black, for: .normal)
-        button_negative.layer.borderColor = UIColor.black.cgColor
-        button_negative.layer.borderWidth = 0.5
-        
-        button_percent.backgroundColor = UIColor.gray
-        button_percent.setTitleColor(.black, for: .normal)
-        button_percent.layer.borderColor = UIColor.black.cgColor
-        button_percent.layer.borderWidth = 0.5
+        nineButton.backgroundColor = UIColor.lightGray
+        nineButton.setTitleColor(.black, for: .normal)
+        nineButton.layer.borderColor = UIColor.black.cgColor
+        nineButton.layer.borderWidth = 0.5
         
         
         
-        button_equal.backgroundColor = UIColor.orange
-        button_equal.setTitleColor(.white, for: .normal)
-        button_equal.layer.borderColor = UIColor.black.cgColor
-        button_equal.layer.borderWidth = 0.5
+        equalButton.backgroundColor = UIColor.orange
+        equalButton.setTitleColor(.white, for: .normal)
+        equalButton.layer.borderColor = UIColor.black.cgColor
+        equalButton.layer.borderWidth = 0.5
         
-        button_plus.backgroundColor = UIColor.orange
-        button_plus.setTitleColor(.white, for: .normal)
-        button_plus.layer.borderColor = UIColor.black.cgColor
-        button_plus.layer.borderWidth = 0.5
+        plusButton.backgroundColor = UIColor.orange
+        plusButton.setTitleColor(.white, for: .normal)
+        plusButton.layer.borderColor = UIColor.black.cgColor
+        plusButton.layer.borderWidth = 0.5
         
-        button_minus.backgroundColor = UIColor.orange
-        button_minus.setTitleColor(.white, for: .normal)
-        button_minus.layer.borderColor = UIColor.black.cgColor
-        button_minus.layer.borderWidth = 0.5
+        minusButton.backgroundColor = UIColor.orange
+        minusButton.setTitleColor(.white, for: .normal)
+        minusButton.layer.borderColor = UIColor.black.cgColor
+        minusButton.layer.borderWidth = 0.5
         
-        button_multi.backgroundColor = UIColor.orange
-        button_multi.setTitleColor(.white, for: .normal)
-        button_multi.layer.borderColor = UIColor.black.cgColor
-        button_multi.layer.borderWidth = 0.5
+        multiButton.backgroundColor = UIColor.orange
+        multiButton.setTitleColor(.white, for: .normal)
+        multiButton.layer.borderColor = UIColor.black.cgColor
+        multiButton.layer.borderWidth = 0.5
         
-        button_divid.backgroundColor = UIColor.orange
-        button_divid.setTitleColor(.white, for: .normal)
-        button_divid.layer.borderColor = UIColor.black.cgColor
-        button_divid.layer.borderWidth = 0.5
+        dividButton.backgroundColor = UIColor.orange
+        dividButton.setTitleColor(.white, for: .normal)
+        dividButton.layer.borderColor = UIColor.black.cgColor
+        dividButton.layer.borderWidth = 0.5
         
         
+        
+        clearButton.backgroundColor = UIColor.gray
+        clearButton.setTitleColor(.black, for: .normal)
+        clearButton.layer.borderColor = UIColor.black.cgColor
+        clearButton.layer.borderWidth = 0.5
+             
+        negativeButton.backgroundColor = UIColor.gray
+        negativeButton.setTitleColor(.black, for: .normal)
+        negativeButton.layer.borderColor = UIColor.black.cgColor
+        negativeButton.layer.borderWidth = 0.5
+             
+        percentButton.backgroundColor = UIColor.gray
+        percentButton.setTitleColor(.black, for: .normal)
+        percentButton.layer.borderColor = UIColor.black.cgColor
+        percentButton.layer.borderWidth = 0.5
+             
         
         
         answer_Label.backgroundColor = UIColor.black
