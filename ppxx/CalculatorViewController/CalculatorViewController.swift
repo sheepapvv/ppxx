20f152a05c77981e46fb3c096b0e2234e7e23ae5
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-10-05 17:06:01 +0900

    #8 comp

--- ppxx/CalculatorViewController/CalculatorViewController.swift
+++ ppxx/CalculatorViewController/CalculatorViewController.swift
@@ -41,70 +41,36 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.buttonConfig1(button: periodButton)
-        self.buttonConfig1(button: zeroButton)
-        self.buttonConfig1(button: oneButton)
-        self.buttonConfig1(button: twoButton)
-        self.buttonConfig1(button: threeButton)
-        self.buttonConfig1(button: fourButton)
-        self.buttonConfig1(button: fiveButton)
-        self.buttonConfig1(button: sixButton)
-        self.buttonConfig1(button: sevenButton)
-        self.buttonConfig1(button: eightButton)
-        self.buttonConfig1(button: nineButton)
+        self.buttonConfig(button: periodButton, color: .lightGray)
+        self.buttonConfig(button: zeroButton, color: .lightGray)
+        self.buttonConfig(button: oneButton, color: .lightGray)
+        self.buttonConfig(button: twoButton, color: .lightGray)
+        self.buttonConfig(button: threeButton, color: .lightGray)
+        self.buttonConfig(button: fourButton, color: .lightGray)
+        self.buttonConfig(button: fiveButton, color: .lightGray)
+        self.buttonConfig(button: sixButton, color: .lightGray)
+        self.buttonConfig(button: sevenButton, color: .lightGray)
+        self.buttonConfig(button: eightButton, color: .lightGray)
+        self.buttonConfig(button: nineButton, color: .lightGray)
         
-        self.buttonConfig2(button: equalButton)
-        self.buttonConfig2(button: plusButton)
-        self.buttonConfig2(button: minusButton)
-        self.buttonConfig2(button: multiButton)
-        self.buttonConfig2(button: dividButton)
+        self.buttonConfig(button: equalButton, color: .orange, titleColor: .white)
+        self.buttonConfig(button: plusButton, color: .orange, titleColor: .white)
+        self.buttonConfig(button: minusButton, color: .orange, titleColor: .white)
+        self.buttonConfig(button: multiButton, color: .orange, titleColor: .white)
+        self.buttonConfig(button: dividButton, color: .orange, titleColor: .white)
         
-        self.buttonConfig3(button: clearButton)
-        self.buttonConfig3(button: negativeButton)
-        self.buttonConfig3(button: percentButton)
-        
-        
-        
-      
+        self.buttonConfig(button: clearButton, color: .gray)
+        self.buttonConfig(button: negativeButton, color: .gray)
+        self.buttonConfig(button: percentButton, color: .gray)
         
         answer_Label.backgroundColor = UIColor.black
         answer_Label.textColor = UIColor.white
-        
-        
-        
-    print("電卓画面")
-    
     }
-    func buttonConfig1 (button: UIButton) {
-        button.backgroundColor = UIColor.lightGray
-        button.setTitleColor(.black, for: .normal)
+    
+    func buttonConfig(button: UIButton, color: UIColor, titleColor: UIColor = .black) {
+        button.backgroundColor = color
+        button.setTitleColor(titleColor, for: .normal)
         button.layer.borderColor = UIColor.black.cgColor
         button.layer.borderWidth = 0.5
     }
-    
-    func buttonConfig2 (button: UIButton) {
-        button.backgroundColor = UIColor.orange
-        button.setTitleColor(.white, for: .normal)
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.borderWidth = 0.5
-    }
-    
-    func buttonConfig3 (button: UIButton) {
-        button.backgroundColor = UIColor.gray
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.borderWidth = 0.5
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
