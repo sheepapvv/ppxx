c859ccc1c7ff1a93e923d34f72d5526be791b392
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2020-10-05 15:51:21 +0900

    コメントの通りに修正

--- ppxx/CalculatorViewController/CalculatorViewController.swift
+++ ppxx/CalculatorViewController/CalculatorViewController.swift
@@ -41,123 +41,60 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        periodButton.backgroundColor = UIColor.lightGray
-        periodButton.setTitleColor(.black, for: .normal)
-        periodButton.layer.borderColor = UIColor.black.cgColor
-        periodButton.layer.borderWidth = 0.5
+        self.buttonConfig1(button: periodButton)
+        self.buttonConfig1(button: zeroButton)
+        self.buttonConfig1(button: oneButton)
+        self.buttonConfig1(button: twoButton)
+        self.buttonConfig1(button: threeButton)
+        self.buttonConfig1(button: fourButton)
+        self.buttonConfig1(button: fiveButton)
+        self.buttonConfig1(button: sixButton)
+        self.buttonConfig1(button: sevenButton)
+        self.buttonConfig1(button: eightButton)
+        self.buttonConfig1(button: nineButton)
         
-        zeroButton.backgroundColor = UIColor.lightGray
-        zeroButton.setTitleColor(.black, for: .normal)
-        zeroButton.layer.borderColor = UIColor.black.cgColor
-        zeroButton.layer.borderWidth = 0.5
+        self.buttonConfig2(button: equalButton)
+        self.buttonConfig2(button: plusButton)
+        self.buttonConfig2(button: minusButton)
+        self.buttonConfig2(button: multiButton)
+        self.buttonConfig2(button: dividButton)
         
-        oneButton.backgroundColor = UIColor.lightGray
-        oneButton.setTitleColor(.black, for: .normal)
-        oneButton.layer.borderColor = UIColor.black.cgColor
-        oneButton.layer.borderWidth = 0.5
-        
-        twoButton.backgroundColor = UIColor.lightGray
-        twoButton.setTitleColor(.black, for: .normal)
-        twoButton.layer.borderColor = UIColor.black.cgColor
-        twoButton.layer.borderWidth = 0.5
-        
-        threeButton.backgroundColor = UIColor.lightGray
-        threeButton.setTitleColor(.black, for: .normal)
-        threeButton.layer.borderColor = UIColor.black.cgColor
-        threeButton.layer.borderWidth = 0.5
-        
-        fourButton.backgroundColor = UIColor.lightGray
-        fourButton.setTitleColor(.black, for: .normal)
-        fourButton.layer.borderColor = UIColor.black.cgColor
-        fourButton.layer.borderWidth = 0.5
-        
-        fiveButton.backgroundColor = UIColor.lightGray
-        fiveButton.setTitleColor(.black, for: .normal)
-        fiveButton.layer.borderColor = UIColor.black.cgColor
-        fiveButton.layer.borderWidth = 0.5
-        
-        sixButton.backgroundColor = UIColor.lightGray
-        sixButton.setTitleColor(.black, for: .normal)
-        sixButton.layer.borderColor = UIColor.black.cgColor
-        sixButton.layer.borderWidth = 0.5
-        
-        sevenButton.backgroundColor = UIColor.lightGray
-        sevenButton.setTitleColor(.black, for: .normal)
-        sevenButton.layer.borderColor = UIColor.black.cgColor
-        sevenButton.layer.borderWidth = 0.5
-        
-        eightButton.backgroundColor = UIColor.lightGray
-        eightButton.setTitleColor(.black, for: .normal)
-        eightButton.layer.borderColor = UIColor.black.cgColor
-        eightButton.layer.borderWidth = 0.5
-        
-        nineButton.backgroundColor = UIColor.lightGray
-        nineButton.setTitleColor(.black, for: .normal)
-        nineButton.layer.borderColor = UIColor.black.cgColor
-        nineButton.layer.borderWidth = 0.5
+        self.buttonConfig3(button: clearButton)
+        self.buttonConfig3(button: negativeButton)
+        self.buttonConfig3(button: percentButton)
         
         
         
-        equalButton.backgroundColor = UIColor.orange
-        equalButton.setTitleColor(.white, for: .normal)
-        equalButton.layer.borderColor = UIColor.black.cgColor
-        equalButton.layer.borderWidth = 0.5
-        
-        plusButton.backgroundColor = UIColor.orange
-        plusButton.setTitleColor(.white, for: .normal)
-        plusButton.layer.borderColor = UIColor.black.cgColor
-        plusButton.layer.borderWidth = 0.5
-        
-        minusButton.backgroundColor = UIColor.orange
-        minusButton.setTitleColor(.white, for: .normal)
-        minusButton.layer.borderColor = UIColor.black.cgColor
-        minusButton.layer.borderWidth = 0.5
-        
-        multiButton.backgroundColor = UIColor.orange
-        multiButton.setTitleColor(.white, for: .normal)
-        multiButton.layer.borderColor = UIColor.black.cgColor
-        multiButton.layer.borderWidth = 0.5
-        
-        dividButton.backgroundColor = UIColor.orange
-        dividButton.setTitleColor(.white, for: .normal)
-        dividButton.layer.borderColor = UIColor.black.cgColor
-        dividButton.layer.borderWidth = 0.5
-        
-        
-        
-        clearButton.backgroundColor = UIColor.gray
-        clearButton.setTitleColor(.black, for: .normal)
-        clearButton.layer.borderColor = UIColor.black.cgColor
-        clearButton.layer.borderWidth = 0.5
-             
-        negativeButton.backgroundColor = UIColor.gray
-        negativeButton.setTitleColor(.black, for: .normal)
-        negativeButton.layer.borderColor = UIColor.black.cgColor
-        negativeButton.layer.borderWidth = 0.5
-             
-        percentButton.backgroundColor = UIColor.gray
-        percentButton.setTitleColor(.black, for: .normal)
-        percentButton.layer.borderColor = UIColor.black.cgColor
-        percentButton.layer.borderWidth = 0.5
-             
-        
+      
         
         answer_Label.backgroundColor = UIColor.black
         answer_Label.textColor = UIColor.white
         
         
         
-        
     print("電卓画面")
     
-    
-    
-    
-    
-    
-    
+    }
+    func buttonConfig1 (button: UIButton) {
+        button.backgroundColor = UIColor.lightGray
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 0.5
     }
     
+    func buttonConfig2 (button: UIButton) {
+        button.backgroundColor = UIColor.orange
+        button.setTitleColor(.white, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 0.5
+    }
+    
+    func buttonConfig3 (button: UIButton) {
+        button.backgroundColor = UIColor.gray
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 0.5
+    }
 
 
     /*
